65657240f448d523c90142d675a8d170408d7a2e
Adriano Godoy <godoy.ccp@gmail.com>
2016-08-17 17:27:26 -0300

    Adds like-search ga profile with underscore

--- src/lib/HubotAnalytics.js
+++ src/lib/HubotAnalytics.js
@@ -1,19 +1,28 @@
+var _ = require("underscore");
+var s = require("underscore.string");
 
 function HubotAnalytics() {
-  this.profilesNames = "";
+  this.profilesNames = {};
 }
 
 HubotAnalytics.prototype.getSiteId = function(siteMatch) {
-  var site = siteMatch;
-  var targetGa = site.replace(/"/g, '');
+  siteMatch = siteMatch.replace(/"/g, '');
 
-  var tryValueByName = this.profilesNames[targetGa];
-  if(tryValueByName != undefined) {
-    targetGa = tryValueByName;
+  var gaByName = this.profilesNames[siteMatch];
+
+  if(gaByName != undefined) {
+    return gaByName; //found by exact name
+  } else {
+    var gaByContainsName = _.find(this.profilesNames, function(item, key){
+                return s.include(key, siteMatch);
+              });
+
+    if(gaByContainsName != undefined) {
+      return gaByContainsName; //found by "like name"
+    } else {
+      return siteMatch; //return inputed ga code
+    }
   }
-
-  return targetGa;
 };
-
 
 module.exports = HubotAnalytics;
